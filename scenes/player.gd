835a2f772cc13775fa5e55636b0676069bdeb261
deltea <thcheetah777@gmail.com>
2025-08-09 00:10:30 -0700

    funi walking animation

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,1 +1,19 @@
 class_name Player extends CharacterBody2D
+
+@export var speed = 50.0
+
+@onready var sprite: Sprite2D = $Sprite
+
+func _ready() -> void:
+	pass
+
+func _process(dt: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction.normalized() * speed
+
+	if direction != Vector2.ZERO:
+		sprite.rotation_degrees = sin(Time.get_ticks_msec() / 50.0) * 12.0
+	else:
+		sprite.rotation_degrees = 0.0
+
+	move_and_slide()
