dc71a38da58251c6857cb7b16157eb1085dc8224
deltea <thcheetah777@gmail.com>
2025-08-09 01:35:30 -0700

    some more ui and selection with mouse

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,6 +2,7 @@ class_name Player extends CharacterBody2D
 
 @export var speed = 50.0
 @export var tilemap: TileMapLayer
+@export var selection_distance = 5
 
 @onready var sprite: Sprite2D = $Sprite
 @onready var selection: Sprite2D = $Selection
@@ -10,7 +11,9 @@ func _ready() -> void:
 	pass
 
 func _process(dt: float) -> void:
-	selection.position = selection.position.lerp(get_tile_pos(get_global_mouse_position()), 20 * dt)
+	var tile_pos = get_tile_pos(position)
+	# var mouse_pos = get_global_mouse_position().clamp(selection_distance * -16.0 * Vector2.ONE, selection_distance * 16.0 * Vector2.ONE)
+	selection.position = selection.position.lerp(get_tile_pos(get_global_mouse_position()).clamp(tile_pos + selection_distance * -16.0 * Vector2.ONE, tile_pos + selection_distance * 16.0 * Vector2.ONE), 20 * dt)
 
 func _physics_process(dt: float) -> void:
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -25,3 +28,9 @@ func _physics_process(dt: float) -> void:
 
 func get_tile_pos(pos: Vector2) -> Vector2:
 	return tilemap.local_to_map(tilemap.to_local(pos)) * 16.0 + Vector2(8, 8)
+
+func clamp_vector(v: Vector2, min: float, max: float) -> Vector2:
+	return Vector2(
+		clamp(v.x, min, max),
+		clamp(v.y, min, max)
+	)
